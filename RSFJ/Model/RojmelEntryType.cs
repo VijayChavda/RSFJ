1b2d9cd3cbd6c8545da5a5b3f1c41136a7bd16d4
Vijay Chavda <VijayChavda@users.noreply.github.com>
2017-06-13 19:47:15 +0530

    Add types of Rojmel entries that will be done.

---
+++ RSFJ/Model/RojmelEntryType.cs
@@ -0,0 +1,38 @@
+﻿namespace RSFJ.Model
+{
+    /// <summary>
+    /// Represents various types of Rojmel entry types.
+    /// </summary>
+    public sealed class RojmelEntryType
+    {
+        /// <summary>
+        /// Newly added stock item.
+        /// </summary>
+        public const string Initital = "Initial";
+
+        /// <summary>
+        /// Exchange of a stock item.
+        /// </summary>
+        public const string Exchange = "Exchange";
+
+        /// <summary>
+        /// Money given/taken for an stock item previously taken/given.
+        /// </summary>
+        public const string Uplak = "Uplak";
+
+        /// <summary>
+        /// Due money calculated and decided for an stock item previously taken/given.
+        /// </summary>
+        public const string UplakClear = "Uplak Clear";
+
+        /// <summary>
+        /// Exchange of fine gold with a bullion.
+        /// </summary>
+        public const string Bullion = "Bullion";
+
+        /// <summary>
+        /// Exchange of a stock item with a customer.
+        /// </summary>
+        public const string Customer = "Customer";
+    }
+}
