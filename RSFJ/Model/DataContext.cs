430044b891d156c1a771ac8fb670b03dd871fa7c
Vijay Chavda <VijayChavda@users.noreply.github.com>
2017-06-13 20:19:07 +0530

    Add Account & RojmelEntryType collections to data context.

--- RSFJ/Model/DataContext.cs
+++ RSFJ/Model/DataContext.cs
@@ -6,11 +6,23 @@ namespace RSFJ.Model
     {
         public HashSet<RojmelEntry> RojmelEntries { get; set; }
         public HashSet<StockItem> StockItems { get; set; }
+        public HashSet<Account> Accounts { get; set; }
+        public HashSet<string> RojmelEntryTypes { get; set; }
 
         public DataContext()
         {
             RojmelEntries = new HashSet<RojmelEntry>();
             StockItems = new HashSet<StockItem>();
+            Accounts = new HashSet<Account>();
+            RojmelEntryTypes = new HashSet<string>()
+            {
+                RojmelEntryType.Initital,
+                RojmelEntryType.Exchange,
+                RojmelEntryType.Uplak,
+                RojmelEntryType.UplakClear,
+                RojmelEntryType.Bullion,
+                RojmelEntryType.Customer,
+            };
         }
     }
 }
