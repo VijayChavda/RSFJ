09aa128850553d6f1fabfd6ae44016d91b82b9c8
Vijay Chavda <VijayChavda@users.noreply.github.com>
2017-06-14 18:04:49 +0530

    Add view & view model for adding new stock items.

---
+++ RSFJ/ViewModels/NewStockItemFlyoutViewModel.cs
@@ -0,0 +1,92 @@
+﻿using RSFJ.ViewModels.Utilities;
+using System.Windows.Input;
+using System;
+using System.Timers;
+
+namespace RSFJ.ViewModels
+{
+    public class NewStockItemFlyoutViewModel : ViewModelBase
+    {
+        private string _Name;
+        public string Name { get => _Name; set => SetProperty(ref _Name, value); }
+
+        private double? _InStock;
+        public double? InStock { get => _InStock; set => SetProperty(ref _InStock, value); }
+
+        private double? _Rate_Purity;
+        public double? Rate_Purity { get => _Rate_Purity; set => SetProperty(ref _Rate_Purity, value); }
+
+        private string _Message;
+        public string Message { get => _Message; set => SetProperty(ref _Message, value); }
+
+        private bool _FlyoutOpen;
+        public bool FlyoutOpen { get => _FlyoutOpen; set => SetProperty(ref _FlyoutOpen, value); }
+
+        RelayCommand _addCommand;
+        public ICommand AddCommand
+        {
+            get => _addCommand ?? (_addCommand = new RelayCommand(param => Add(), param => true));
+        }
+
+        private void Add()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Message = "Please provide a name to this item.";
+                return;
+            }
+
+            if (Rate_Purity == null)
+            {
+                Message = "Please provide a valid value for purity.";
+                return;
+            }
+
+            if (InStock == null)
+            {
+                Message = "Please provide a valid initial amount of stock.";
+                return;
+            }
+
+            var added = Services.DataContextService.Instance.DataContext.StockItems.Add(new Model.StockItem()
+            {
+                Name = Name,
+                InStock = (double)InStock,
+                Rate_Purity = (double)Rate_Purity,
+                EquivalentGold = (double)(InStock * Rate_Purity)
+            });
+
+            if (added)
+            {
+                Message = "Item was added successfully...";
+
+                var timer = new Timer(800) { AutoReset = false };
+                timer.Elapsed += (sender, e) => FlyoutOpen = !true;
+                timer.Start();
+            }
+            else
+            {
+                Message = "This item already exists. Please provide unique names for stock items.";
+            }
+        }
+
+        protected override void APropertyChanged<T>(string PropertyName, T OldValue, T NewValue)
+        {
+            if (string.IsNullOrWhiteSpace(Name) == false && Rate_Purity != null && InStock != null)
+            {
+                Message = Message == "Item was added successfully..." ? Message : null;
+            }
+
+            if (PropertyName == nameof(FlyoutOpen))
+            {
+                if (FlyoutOpen)
+                {
+                    Name = null;
+                    InStock = null;
+                    Rate_Purity = null;
+                    Message = null;
+                }
+            }
+        }
+    }
+}
