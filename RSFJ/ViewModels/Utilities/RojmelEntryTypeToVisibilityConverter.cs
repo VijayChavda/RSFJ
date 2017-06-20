78b447c06031300458517d0b35d08d0e212be100
Vijay Chavda <VijayChavda@users.noreply.github.com>
2017-06-20 16:45:12 +0530

    Modify details visibility settings.

---
+++ RSFJ/ViewModels/Utilities/RojmelEntryTypeToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RSFJ.ViewModels.Utilities
+{
+    public class RojmelEntryTypeToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value == null)
+            {
+                return Visibility.Collapsed;
+            }
+
+            var type = value.ToString();
+            var typeToComparewith = parameter?.ToString();
+
+            return type == typeToComparewith ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
