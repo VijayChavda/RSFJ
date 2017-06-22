23269fbd5b20d05fe42123ffef13b5c6e6ab93ce
Vijay Chavda <VijayChavda@users.noreply.github.com>
2017-06-22 14:25:00 +0530

    Fix - DataGrid red border error.

---
+++ RSFJ/ViewModels/Utilities/RojmelEntryViewModelTypeToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RSFJ.ViewModels.Utilities
+{
+    public class RojmelEntryViewModelTypeToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is RojmelEntryViewModel ? value : null;
+        }
+    }
+}
