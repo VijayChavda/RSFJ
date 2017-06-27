20c35292caecf0d14ce778bfda235a6e9d18b685
Vijay Chavda <VijayChavda@users.noreply.github.com>
2017-06-27 22:36:39 +0530

    Modify customer parameters visibility.

---
+++ RSFJ/ViewModels/Utilities/CustomerEntryVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using RSFJ.Model;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RSFJ.ViewModels.Utilities
+{
+    public class CustomerEntryVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || value.GetType() != typeof(AccountType))
+            {
+                return Visibility.Collapsed;
+            }
+
+            return (AccountType)value == AccountType.Customer ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
