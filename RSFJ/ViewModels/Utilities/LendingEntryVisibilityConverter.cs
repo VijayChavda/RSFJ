68577944c4c13c5fb80fc79ab9e1685164a96b02
Vijay Chavda <VijayChavda@users.noreply.github.com>
2017-06-27 22:26:01 +0530

    UI modifications with lending entries.

---
+++ RSFJ/ViewModels/Utilities/LendingEntryVisibilityConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RSFJ.ViewModels.Utilities
+{
+    public class LendingEntryVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var entry = value as RojmelEntryViewModel;
+
+            if (entry == null)
+                return Visibility.Collapsed;
+
+            if (entry.Type == Model.RojmelEntryType.ItemExchangeFine && !entry.IsLeftSide)
+            {
+                return Visibility.Visible;
+            }
+
+            if (entry.Type == Model.RojmelEntryType.ItemExchangeCash && entry.IsLeftSide)
+            {
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
