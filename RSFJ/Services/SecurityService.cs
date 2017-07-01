292f0cea00df8c4410f8653995d7a73c483fdc6b
Vijay Chavda <VijayChavda@users.noreply.github.com>
2017-07-01 17:02:02 +0530

    Add SecurityService.

---
+++ RSFJ/Services/SecurityService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSFJ.Services
+{
+    /// <summary>
+    /// Service that provides various security features.
+    /// </summary>
+    public class SecurityService
+    {
+        /// <summary>
+        /// The only instance of SecurityService.
+        /// </summary>
+        public static SecurityService Instance => _Instance ?? (_Instance = new SecurityService());
+        private static SecurityService _Instance;
+
+        /// <summary>
+        /// Computes a hash for the given string.
+        /// </summary>
+        /// <param name="secret">A string whose hash is to be computed.</param>
+        /// <returns></returns>
+        public string Hash(string secret)
+        {
+            if (String.IsNullOrEmpty(secret))
+                return String.Empty;
+
+            using (var sha = new SHA256Managed())
+            {
+                byte[] textData = Encoding.UTF8.GetBytes(secret);
+                byte[] hash = sha.ComputeHash(textData);
+                return BitConverter.ToString(hash).Replace("-", String.Empty);
+            }
+        }
+    }
+}
