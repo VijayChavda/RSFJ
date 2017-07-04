7915abcc2a45a362ce8450f3769fa8a431187557
Vijay Chavda <VijayChavda@users.noreply.github.com>
2017-07-04 12:05:07 +0530

    Add entries from EntriesPage.

--- RSFJ/ViewModels/EntriesViewModel.cs
+++ RSFJ/ViewModels/EntriesViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using RSFJ.Model;
+using RSFJ.ViewModels.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace RSFJ.ViewModels
 {
@@ -57,6 +59,20 @@ namespace RSFJ.ViewModels
         public FineClearWithAccountBalanceViewModel FineClearWithAccountBalanceViewModel { get; set; }
         public FineClearViewModel FineClearViewModel { get; set; }
 
+        #region Commands
+        RelayCommand _CreditCommand;
+        public ICommand CreditCommand
+        {
+            get => _CreditCommand ?? (_CreditCommand = new RelayCommand(param => AddEntry(true), param => true));
+        }
+
+        RelayCommand _DebitCommand;
+        public ICommand DebitCommand
+        {
+            get => _DebitCommand ?? (_DebitCommand = new RelayCommand(param => AddEntry(false), param => true));
+        }
+        #endregion
+
         public ExchangeEntryViewModel()
         {
             Accounts = new List<Account>();
@@ -75,8 +91,205 @@ namespace RSFJ.ViewModels
             Accounts.AddRange(Services.DataContextService.Instance.DataContext.Accounts.Where(x => x.Type == AccountType.Regular));
             Account = Accounts.FirstOrDefault();
         }
+
+        private void AddEntry(bool OnLeftSide)
+        {
+            var entry = new RojmelEntry()
+            {
+                Id = Services.DataContextService.Instance.DataContext.RojmelEntries.Count + 1,
+                Account = Account,
+                Date = DateTime.Now,
+                IsLeftSide = OnLeftSide
+            };
+
+            if (IsExchangeWithFine)
+            {
+                entry.Type = RojmelEntryType.ItemExchangeFine;
+                entry.StockItem = ExchangeWithFineViewModel.StockItem;
+                entry.Param1 = ExchangeWithFineViewModel.Weight;
+                entry.Param2 = ExchangeWithFineViewModel.Purity;
+                entry.FullPaymentDueDays = DateTime.Now.Subtract(ExchangeWithFineViewModel.PaymentBefore).Days;
+                entry.PartialPaymentInterval = DateTime.Now.Subtract(ExchangeWithFineViewModel.PaymentInterval).Days;
+            }
+            else if (IsCashPayment)
+            {
+                entry.Type = RojmelEntryType.SimpleCashExchange;
+                entry.StockItem = StockItem.Cash;
+                entry.Param1 = CashPaymentViewModel.Cash;
+            }
+            else if (IsFineClearWithAccountBalance)
+            {
+                entry.Type = RojmelEntryType.UseCash;
+                entry.StockItem = StockItem.None;
+                entry.Param1 = FineClearWithAccountBalanceViewModel.AccountBalance;
+                entry.Param2 = FineClearWithAccountBalanceViewModel.Rate;
+            }
+            else if (IsFineClear)
+            {
+                entry.Type = RojmelEntryType.ItemExchangeCash;
+                entry.StockItem = StockItem.Cash;
+                entry.Param1 = FineClearViewModel.Cash;
+                entry.Param2 = FineClearViewModel.Rate;
+            }
+
+            Services.DataContextService.Instance.DataContext.RojmelEntries.Add(entry);
+        }
     }
 
+    public class BullionEntryViewModel : ViewModelBase
+    {
+        public static List<Account> Accounts { get; set; }
+
+        private Account _Account;
+        public Account Account { get => _Account; set => SetProperty(ref _Account, value); }
+
+        private bool _IsFine999Payment;
+        public bool IsFine999Payment { get => _IsFine999Payment; set => SetProperty(ref _IsFine999Payment, value); }
+
+        private bool _IsCashPayment;
+        public bool IsCashPayment { get => _IsCashPayment; set => SetProperty(ref _IsCashPayment, value); }
+
+        public Fine999PaymentViewModel Fine999PaymentViewModel { get; set; }
+        public CashPaymentViewModel CashPaymentViewModel { get; set; }
+
+        #region Commands
+        RelayCommand _CreditCommand;
+        public ICommand CreditCommand
+        {
+            get => _CreditCommand ?? (_CreditCommand = new RelayCommand(param => AddEntry(true), param => true));
+        }
+
+        RelayCommand _DebitCommand;
+        public ICommand DebitCommand
+        {
+            get => _DebitCommand ?? (_DebitCommand = new RelayCommand(param => AddEntry(false), param => true));
+        }
+        #endregion
+
+        public BullionEntryViewModel()
+        {
+            Accounts = new List<Account>();
+
+            Fine999PaymentViewModel = new Fine999PaymentViewModel();
+            CashPaymentViewModel = new CashPaymentViewModel();
+
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Accounts.Clear();
+            Accounts.AddRange(Services.DataContextService.Instance.DataContext.Accounts.Where(x => x.Type == AccountType.Boolean));
+            Account = Accounts.FirstOrDefault();
+        }
+
+        private void AddEntry(bool OnLeftSide)
+        {
+            var entry = new RojmelEntry()
+            {
+                Id = Services.DataContextService.Instance.DataContext.RojmelEntries.Count + 1,
+                Account = Account,
+                Date = DateTime.Now,
+                IsLeftSide = OnLeftSide
+            };
+
+            if (IsFine999Payment)
+            {
+                entry.Type = RojmelEntryType.ItemExchangeCash;
+                entry.StockItem = StockItem.Fine999;
+                entry.Param1 = Fine999PaymentViewModel.Weight;
+                entry.Param2 = Fine999PaymentViewModel.Rate;
+                entry.FullPaymentDueDays = DateTime.Now.Subtract(Fine999PaymentViewModel.PaymentBefore).Days;
+                entry.PartialPaymentInterval = DateTime.Now.Subtract(Fine999PaymentViewModel.PaymentInterval).Days;
+            }
+            else if (IsCashPayment)
+            {
+                entry.Type = RojmelEntryType.SimpleCashExchange;
+                entry.StockItem = StockItem.Cash;
+                entry.Param1 = CashPaymentViewModel.Cash;
+            }
+
+            Services.DataContextService.Instance.DataContext.RojmelEntries.Add(entry);
+        }
+    }
+
+    public class CustomerEntryViewModel : ViewModelBase
+    {
+        public static List<Account> Accounts { get; set; }
+
+        private Account _Account;
+        public Account Account { get => _Account; set => SetProperty(ref _Account, value); }
+
+        private StockItem _StockItem;
+        public StockItem StockItem { get => _StockItem; set => SetProperty(ref _StockItem, value); }
+
+        private double _Weight;
+        public double Weight { get => _Weight; set => SetProperty(ref _Weight, value); }
+
+        private double _Rate;
+        public double Rate { get => _Rate; set => SetProperty(ref _Rate, value); }
+
+        private double _Labour;
+        public double Labour { get => _Labour; set => SetProperty(ref _Labour, value); }
+
+        private double _Waste;
+        public double Waste { get => _Waste; set => SetProperty(ref _Waste, value); }
+
+        private bool _IsLabourAsAmount;
+        public bool IsLabourAsAmount { get => _IsLabourAsAmount; set => SetProperty(ref _IsLabourAsAmount, value); }
+
+        #region Commands
+        RelayCommand _CreditCommand;
+        public ICommand CreditCommand
+        {
+            get => _CreditCommand ?? (_CreditCommand = new RelayCommand(param => AddEntry(true), param => true));
+        }
+
+        RelayCommand _DebitCommand;
+        public ICommand DebitCommand
+        {
+            get => _DebitCommand ?? (_DebitCommand = new RelayCommand(param => AddEntry(false), param => true));
+        }
+        #endregion
+
+        public CustomerEntryViewModel()
+        {
+            Accounts = new List<Account>();
+
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Accounts.Clear();
+            Accounts.AddRange(Services.DataContextService.Instance.DataContext.Accounts.Where(x => x.Type == AccountType.Customer));
+            Account = Accounts.FirstOrDefault();
+
+            StockItem = EntriesViewModel.StockItems.FirstOrDefault();
+        }
+
+        private void AddEntry(bool OnLeftSide)
+        {
+            var entry = new RojmelEntry()
+            {
+                Id = Services.DataContextService.Instance.DataContext.RojmelEntries.Count + 1,
+                Type = RojmelEntryType.ItemExchangeCash,
+                Account = Account,
+                StockItem = StockItem,
+                Date = DateTime.Now,
+                IsLeftSide = OnLeftSide,
+                IsLabourAsAmount = IsLabourAsAmount,
+                Labour = Labour,
+                Param1 = Weight,
+                Param2 = Rate,
+                Waste = Waste
+            };
+
+            Services.DataContextService.Instance.DataContext.RojmelEntries.Add(entry);
+        }
+    }
+
+    #region Sub types
     public class ExchangeWithFineViewModel : ViewModelBase
     {
         private StockItem _StockItem;
@@ -126,40 +339,6 @@ namespace RSFJ.ViewModels
         public double Rate { get => _Rate; set => SetProperty(ref _Rate, value); }
     }
 
-    public class BullionEntryViewModel : ViewModelBase
-    {
-        public static List<Account> Accounts { get; set; }
-
-        private Account _Account;
-        public Account Account { get => _Account; set => SetProperty(ref _Account, value); }
-
-        private bool _IsFine999Payment;
-        public bool IsFine999Payment { get => _IsFine999Payment; set => SetProperty(ref _IsFine999Payment, value); }
-
-        private bool _IsCashPayment;
-        public bool IsCashPayment { get => _IsCashPayment; set => SetProperty(ref _IsCashPayment, value); }
-
-        public Fine999PaymentViewModel Fine999PaymentViewModel { get; set; }
-        public CashPaymentViewModel CashPaymentViewModel { get; set; }
-
-        public BullionEntryViewModel()
-        {
-            Accounts = new List<Account>();
-
-            Fine999PaymentViewModel = new Fine999PaymentViewModel();
-            CashPaymentViewModel = new CashPaymentViewModel();
-
-            LoadData();
-        }
-
-        private void LoadData()
-        {
-            Accounts.Clear();
-            Accounts.AddRange(Services.DataContextService.Instance.DataContext.Accounts.Where(x => x.Type == AccountType.Boolean));
-            Account = Accounts.FirstOrDefault();
-        }
-    }
-
     public class Fine999PaymentViewModel : ViewModelBase
     {
         private double _Weight;
@@ -167,47 +346,12 @@ namespace RSFJ.ViewModels
 
         private double _Rate;
         public double Rate { get => _Rate; set => SetProperty(ref _Rate, value); }
+
+        private DateTime _PaymentBefore;
+        public DateTime PaymentBefore { get => _PaymentBefore; set => SetProperty(ref _PaymentBefore, value); }
+
+        private DateTime _PaymentInterval;
+        public DateTime PaymentInterval { get => _PaymentInterval; set => SetProperty(ref _PaymentInterval, value); }
     }
-
-    public class CustomerEntryViewModel : ViewModelBase
-    {
-        public static List<Account> Accounts { get; set; }
-
-        private Account _Account;
-        public Account Account { get => _Account; set => SetProperty(ref _Account, value); }
-
-        private StockItem _StockItem;
-        public StockItem StockItem { get => _StockItem; set => SetProperty(ref _StockItem, value); }
-
-        private double _Weight;
-        public double Weight { get => _Weight; set => SetProperty(ref _Weight, value); }
-
-        private double _Rate;
-        public double Rate { get => _Rate; set => SetProperty(ref _Rate, value); }
-
-        private double _Labour;
-        public double Labour { get => _Labour; set => SetProperty(ref _Labour, value); }
-
-        private double _Waste;
-        public double Waste { get => _Waste; set => SetProperty(ref _Waste, value); }
-
-        private bool _IsLabourAsAmount;
-        public bool IsLabourAsAmount { get => _IsLabourAsAmount; set => SetProperty(ref _IsLabourAsAmount, value); }
-
-        public CustomerEntryViewModel()
-        {
-            Accounts = new List<Account>();
-
-            LoadData();
-        }
-
-        private void LoadData()
-        {
-            Accounts.Clear();
-            Accounts.AddRange(Services.DataContextService.Instance.DataContext.Accounts.Where(x => x.Type == AccountType.Customer));
-            Account = Accounts.FirstOrDefault();
-
-            StockItem = EntriesViewModel.StockItems.FirstOrDefault();
-        }
-    }
+    #endregion
 }
