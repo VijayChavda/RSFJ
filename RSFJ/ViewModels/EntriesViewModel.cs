2daf1192affa27845dbd1a7e85469a71ac9a1a85
Vijay Chavda <VijayChavda@users.noreply.github.com>
2017-08-15 16:38:45 +0530

    Rewrite properly.

--- RSFJ/ViewModels/EntriesViewModel.cs
+++ RSFJ/ViewModels/EntriesViewModel.cs
@@ -57,13 +57,13 @@ namespace RSFJ.ViewModels
         private void LoadData()
         {
             StockItems.Clear();
-            foreach (var item in DataContextService.Instance.DataContext.StockItems.Except(
-                new StockItem[] {
-                    DataContextService.Instance.DataContext.Cash,
-                    DataContextService.Instance.DataContext.None
-                }))
+            foreach (var item in DataContextService.Instance.DataContext.StockItems)
             {
-                StockItems.Add(item);
+                if (item == DataContextService.Instance.DataContext.Cash ||
+                    item == DataContextService.Instance.DataContext.None)
+                {
+                    StockItems.Add(item);
+                }
             }
         }
 
