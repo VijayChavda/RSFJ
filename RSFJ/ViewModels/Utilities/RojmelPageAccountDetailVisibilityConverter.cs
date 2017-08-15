d55d526965654e8349e28bbc11057ccf6122227b
Vijay Chavda <VijayChavda@users.noreply.github.com>
2017-08-15 17:35:37 +0530

    RojmelPage Account detail visibility.

---
+++ RSFJ/ViewModels/Utilities/RojmelPageAccountDetailVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RSFJ.ViewModels.Utilities
+{
+    public class RojmelPageAccountDetailVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is RojmelEntryViewModel entry)
+            {
+                if (entry.Account == null || entry.Account.Type == Model.AccountType.Self)
+                {
+                    return Visibility.Collapsed;
+                }
+
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
