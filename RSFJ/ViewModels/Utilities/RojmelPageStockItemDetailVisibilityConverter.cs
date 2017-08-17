0b00581f4df9786a6b7dc1afbf98ef9ab50b5892
Vijay Chavda <VijayChavda@users.noreply.github.com>
2017-08-17 14:06:40 +0530

    Hide stock item detail for None item.

---
+++ RSFJ/ViewModels/Utilities/RojmelPageStockItemDetailVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using RSFJ.Services;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RSFJ.ViewModels.Utilities
+{
+    public class RojmelPageStockItemDetailVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is RojmelEntryViewModel entry)
+            {
+                if (entry.StockItem == null || entry.StockItem.Name == DataContextService.Instance.DataContext.None.Name)
+                {
+                    return Visibility.Collapsed;
+                }
+
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
